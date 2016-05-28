db10e6b9f47919bded09967230101259b9e0c54c
techgeek99 <satyabrat.me@gmail.com>
2016-05-28 22:25:53 +0530

    Initial Commit

---
+++ setupenv.sh
@@ -0,0 +1,9 @@
+echo Setting Up Build Environment
+sudo apt-get install bison build-essential curl flex git gnupg gperf libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 libxml2-utils lzop maven openjdk-7-jdk pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev g++-multilib gcc-multilib lib32ncurses5-dev lib32readline-gplv2-dev lib32z1-dev jack phablet-tools
+echo Installing Dependencies Done
+echo ...
+echo Installing repo binary
+mkdir -p ~/bin
+mkdir -p ~/android/system
+echo Build Environment Ready
+echo Start Your Build and Enjoy 
