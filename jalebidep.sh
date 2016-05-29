cbda8ea3a3fe771b1383b746188366d8b752b70a
Satyabrat Sahoo <satyabrat.me@gmail.com>
2016-05-29 11:45:11 +0530

    Initial Commit

---
+++ jalebidep.sh
@@ -0,0 +1,25 @@
+red='tput setaf 1'              # Red
+green='tput setaf 2'            # Green
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) # Bold Red
+bldgrn=${txtbld}$(tput setaf 2) # Bold Green
+tput setaf 1
+echo Cleaning Directories...
+tput setaf 2
+rm -rf device/yu/jalebi device/cyanogen/msm8916-common kernel/ vendor/yu
+tput setaf 1
+echo Cloning Jalebi Device Tree
+tput setaf 2
+git clone https://github.com/CyanogenMod/android_device_yu_jalebi -b cm-13.0 device/yu/jalebi/
+tput setaf 1
+echo Cloning msm8916 common
+tput setaf 2
+git clone https://github.com/CyanogenMod/android_device_cyanogen_msm8916-common -b cm-13.0 device/cyanogen/msm8916-common/
+tput setaf 1
+echo Cloning msm8916 kernel
+tput setaf 2
+git clone https://github.com/CyanogenMod/android_kernel_cyanogen_msm8916 -b cm-13.0 kernel/cyanogen/msm8916/
+tput setaf 1
+echo Cloning Jalebi Vendor
+tput setaf 2
+git clone https://github.com/FireLord1/android_vendor_yu_jalebi -b cm-13.0 vendor/yu/jalebi/
