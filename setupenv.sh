4a01e14f6560aab92b8c02bcf6d5e6e89b05424d
Satyabrat Sahoo <satyabrat.me@gmail.com>
2016-06-12 09:46:18 +0530

    Added libstdc++ dependencies

--- setupenv.sh
+++ setupenv.sh
@@ -5,6 +5,9 @@ tput setaf 1
 echo Setting Up Build Environment
 tput setaf 2
 sudo apt-get install bison build-essential curl flex git gnupg gperf libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 libxml2-utils lzop maven openjdk-7-jdk pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev g++-multilib gcc-multilib lib32ncurses5-dev lib32readline-gplv2-dev lib32z1-dev jack phablet-tools
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install libstdc++6
 tput setaf 1
 echo Installing Dependencies Done
 echo ...
