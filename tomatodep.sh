72b6e7cc13115f0c2d667ba405e3b5f019cef035
Satyabrat Sahoo <satyabrat.me@gmail.com>
2016-06-22 11:44:39 +0530

    Create tomatodep.sh

---
+++ tomatodep.sh
@@ -0,0 +1,14 @@
+green='tput setaf 2'            # Green
+white='tput setaf 7'            # White
+tput setaf 2
+echo Cleaning Directories...
+rm -rf device/yu/ device/google/sprout4 device/google/sprout-common device/Cyanogen/msm8916-common kernel/ vendor/yu vendor/google
+echo Cloning Jalebi Stuffs
+git clone https://github.com/karthick111/android_device_cyanogen_msm8916-common -b cm-13.0 device/cyanogen/msm8916-common
+git clone https://github.com/karthick111/android_device_yu_tomato -b cm-13.0 device/yu/tomato
+git clone https://github.com/TheMuppets/proprietary_vendor_yu -b cm-13.0 vendor/yu/
+git clone https://github.com/karthick111/hyper_tomato -b staging/cm-13.0-lettuce kernel/cyanogen/msm8916
+rm -rf device/qcom/common
+git clone https://github.com/CyanogenMod/android_device_qcom_common -b cm-13.0 device/qcom/common
+echo Cloning Done Now Enjoy XD
+tput setaf 7
