0689360476f3f222c8c529fe3f51ab438b3b1350
Jashaswee Jena <jishujena2000@gmail.com>
2016-07-07 10:37:35 +0530

    Fix kenzo shits

--- kenzo.sh
+++ kenzo.sh
@@ -4,7 +4,7 @@ tput setaf 2
 echo Cleaning Directories...
 echo Cloning Kenzo Stuffs
 git clone https://github.com/TheStrix/android_device_xiaomi_kenzo.git -b cm-13.0 device/xiaomi/kenzo
-git clone https://github.com/TheStrix/proprietary_vendor_xiaomi.git -b cm-13.0 vendor/xiaomi/kenzo
+git clone https://github.com/TheStrix/proprietary_vendor_xiaomi.git -b cm-13.0 vendor/xiaomi
 git clone https://github.com/TheStrix/android_kernel_xiaomi_msm8956.git -b cm-13.0 kernel/xiaomi/msm8956
 rm -rf device/qcom/common
 git clone https://github.com/CyanogenMod/android_device_qcom_common -b cm-13.0 device/qcom/common
